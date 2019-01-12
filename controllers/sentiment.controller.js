f5b9307499d3057f9c49f2f04e64690dc46b3208
Sivakumar M <sivakumar.m01@sap.com>
2019-01-13 03:07:43 +0530

    Changed the type of neural network

--- controllers/sentiment.controller.js
+++ controllers/sentiment.controller.js
@@ -1,6 +1,18 @@
 const brain = require('brain.js');
 const fs = require('fs');
-const nNetwork = new brain.recurrent.LSTM();
+const nNetwork = new brain.NeuralNetwork();
+
+function encode(arg){
+    return arg.split('').map(x => (x.charCodeAt(0) / 255));
+}
+
+function fixLengths(data) {
+    let maxLengthInput = 144;    
+    while (data.length < maxLengthInput) {
+        data.push(0);
+    }
+    return data;
+}
 
 module.exports = {
     analyzeText: function(req, res){
@@ -8,20 +20,14 @@ module.exports = {
         const trainedModel  = fs.readFileSync('./ml/tweetsentiment.model.json', 'utf8');
         nNetwork.fromJSON(JSON.parse(trainedModel));
 
-        const output = nNetwork.run(req.body.text);
-        let sentiment;
-        switch(output){
-            case "N":
-                sentiment = "Negative";
-                break;
-            case "P":
-                sentiment = "Positive";
-                break;
-            default:
-                sentiment = "Neutral";
-        }
+        const output = nNetwork.run(fixLengths(encode(req.body.text)));
+        let sentiment = {
+            Positive :  Math.floor(output.Positive * 100) + "%",
+            Negative :  Math.floor(output.Negative * 100) + "%",
+            Neutral :  Math.floor(output.Neutral * 100) + "%"
+        };
 
         res.status(200);
-        res.json({status : "API is brewing", data: sentiment});
+        res.json({sentiment: sentiment});
     }
 }
--- controllers/train.controller.js
+++ controllers/train.controller.js
@@ -1,20 +1,42 @@
 const brain = require('brain.js');
 const fs = require('fs');
 const data = require('../data/training.data.json');
-const nNetwork = new brain.recurrent.LSTM();
+const nNetwork = new brain.NeuralNetwork();
+
+function encode(arg){
+    return arg.split('').map(x => (x.charCodeAt(0) / 255));
+}
+
+function fixLengths(data) {
+    let maxLengthInput = -1;
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].input.length > maxLengthInput) {
+        maxLengthInput = data[i].input.length;
+      }
+    }
+    
+    for (let i = 0; i < data.length; i++) {
+      while (data[i].input.length < maxLengthInput) {
+        data[i].input.push(0);
+      }
+    }
+    
+    console.log(maxLengthInput);
+    return data;
+}
 
 module.exports = {
 
     trainData: function(req, res){
-        console.log("Before Processing....");
+        //const data1 = fixLengths(data);
         const trainingData = data.map(item => ({
-            input: item.input,
+            input: encode(item.input),
             output: item.output
         }));
 
         console.log("Before Training....");
-        nNetwork.train(trainingData, {
-            iterations: 1000,
+        nNetwork.train(fixLengths(trainingData), {
+            iterations: 2000,
             log: (error) => {
                 console.log(error);
             },
